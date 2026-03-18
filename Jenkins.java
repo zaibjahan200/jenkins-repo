970c851f873d0204e6ca10f97c942a7f053a564c
Jahanzaib Piracha <zaibjahan.200@gmail.com>
2026-03-18 17:22:52 +0500

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -1,5 +1,6 @@
 public class Jenkins {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins! This is Jahanzaib");
+        System.out.println("Hello, Jenkins! This is Jahanzaib.");
+        System.out.println("Build Successfully?");
     }
 }
